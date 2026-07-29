f8e04ce9b990335f346f972a33e7b713e6f2498d
AlbertoSocorro <alsocorrogallardo@gmail.com>
2026-07-29 14:03:30 +0100

    feat: añadir migraciones para mensaje de contacto

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,5 +7,5 @@ app_name = "portfolio"
 urlpatterns = [
     path("", portfolio_home, name="home"),
     path("filtrar-proyectos/", filtrar_proyectos, name="filtrar_proyectos"),
-    path('contacto/', procesar_contacto, name='contacto'),
+    path("contacto/", procesar_contacto, name="contacto"),
 ]
